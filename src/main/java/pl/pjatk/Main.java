a802a33746e9ad508fcfcf341e9da42f0c5995fb
Piotr <s21911@pjwstk.edu.pl>
2020-05-30 16:56:26 +0200

    sprawdzam

--- src/main/java/pl/pjatk/Main.java
+++ src/main/java/pl/pjatk/Main.java
@@ -6,11 +6,19 @@ public class Main {
         Account account = new Account (1,"Peter","Mastalerz",2000,1);
         Account account1 = new Account (2000,2);
         Account account2 = new Account (2,"Pawel","Mastalerz",1500,2);
+
+
         System.out.println (account1.getBalance ());
         System.out.println (account1.getAccountId ());
-        System.out.println (account2.getBalance ());
-        System.out.println (account.getSurname ());
+
         System.out.println (account2.getName ());
+        System.out.println (account2.getBalance ());
+
+
+        System.out.println (account.getSurname ());
+
+
+
         account1.setBalanceAccount (100);
         account1.setBalanceAccount (250);
         account1.setBalanceAccount (400);
